39ee750b12c7fd65924e5a63b17937f27e364dd8
Pogues2K <75917492+Pogues2K@users.noreply.github.com>
2022-01-17 16:09:12 -0500

    Add files via upload

---
+++ password_generator.py
@@ -0,0 +1,41 @@
+# Password Generator Project
+import random
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+           'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+
+password_list = []
+
+for char in range(0, nr_letters + 1):
+    # This picks random letters from the user
+    randomLetters = random.choice(letters)
+    password_list += randomLetters
+
+
+for num in range(0, nr_numbers + 1):
+    # This picks random numbers from the user
+    randomNumbers = random.choice(numbers)
+    password_list += randomNumbers
+
+
+for sym in range(0, nr_symbols + 1):
+    # This picks random symbols from the user
+    randomSymbols = random.choice(symbols)
+    password_list += randomSymbols
+
+random.shuffle(password_list)  # This shuffles the password
+
+
+password = ""  # This convers the password to string
+for character in password_list:
+    password += character
+print(f"Your brand new password is: {password}")
